ecd3d767739d0c2d57cfacdeb88f140b4e26395c
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-02 08:51:57 -0300

    get()

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get()
+  findAll() {
+    return 'Listagem de cursos';
+  }
+}
