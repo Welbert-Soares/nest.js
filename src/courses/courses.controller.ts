d9c5df83badb3c033f357a0c98cf925301f4d985
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-02 09:02:23 -0300

    decoretion com paramentros

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll() {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
   }
 }
