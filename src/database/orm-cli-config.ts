b9779f2fa0d1727e7e9a8860c71cac9c73ee764e
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-04 09:57:17 -0300

    criando tabela tags com migtration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1714825778133 } from 'src/migrations/1714825778133-CreateCoursesTable';
+import { CreateTagsTable1714827026822 } from 'src/migrations/1714827026822-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1714825778133],
+  migrations: [CreateCoursesTable1714825778133, CreateTagsTable1714827026822],
 });
