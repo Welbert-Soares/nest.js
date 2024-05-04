6ac6e43ae85cf38c8c7e12b6827955e921241542
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-04 10:50:52 -0300

    migrando coluna para relacionamento com a tabela courses

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1714825778133 } from 'src/migrations/1714825778133-CreateCoursesTable';
 import { CreateTagsTable1714827026822 } from 'src/migrations/1714827026822-CreateTagsTable';
 import { CreateCoursesTagsTable1714828597631 } from 'src/migrations/1714828597631-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1714829447972 } from 'src/migrations/1714829447972-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1714825778133,
     CreateTagsTable1714827026822,
     CreateCoursesTagsTable1714828597631,
+    AddCoursesIdToCoursesTagsTable1714829447972,
   ],
 });
